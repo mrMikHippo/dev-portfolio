a813c27a59dfb23bd1e1a6d86b65ff877516f0ec
mrMikOcto <mik.hippo@yandex.ru>
2018-10-30 20:28:22 +0300

    Implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,11 @@
 class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+	# Create a custom scopes
+	
+	def self.math_integrals
+		where(subtitle: "Math Integrals")
+	end
+
+	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") } 
 end
