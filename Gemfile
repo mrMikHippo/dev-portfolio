deafe3929ea3e43aa99ce13e5609e6bd1f7762aa
mrMikOcto <mik.hippo@yandex.ru>
2018-11-27 19:36:34 +0300

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'friendly_id', '~> 5.2.0'
 gem 'devise', '~> 4.5'
 
 gem 'bootstrap', '~> 4.1.3'
+
+gem 'hippo_view_tool', git: 'https://github.com/mrMikHippo/hippo_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,8 @@ module ApplicationHelper
       		content_tag(:p, greeting, class: "source-greeting")
     	end		
 	end
+
+	def copyright_generator
+		HippoViewTool::Renderer.copyright 'mr. Mik Hippo', 'All rights reserved'
+	end
 end
